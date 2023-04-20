9deb12aa736f63bac884d6d727624e99d9df95d8
heitor2002 <montagnini.heitor@gmail.com>
2023-04-20 14:32:33 -0300

    refactor: removendo dependência em desuso.

--- back-projeto-arquitetura/models/Client.js
+++ back-projeto-arquitetura/models/Client.js
@@ -8,5 +8,29 @@ const Client = db.define("data_clients", {
         autoIncrement: true,
         allowNull: false,
         primaryKey: true
-    }
+    },
+    name:{
+        type:Sequelize.STRING,
+        allowNull: false
+    },
+    email:{
+        type:Sequelize.STRING,
+        allowNull: false
+    },
+    phone:{
+        type:Sequelize.STRING,
+        allowNull: false
+    },
+    city:{
+        type:Sequelize.STRING,
+        allowNull: false
+    },
+    subject:{
+        type:Sequelize.STRING,
+        allowNull: false
+    },
+    message:{
+        type:Sequelize.STRING,
+        allowNull: false
+    },
 })
