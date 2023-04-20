87dd182a17ffa2d0cf9b371c5aa5f8e1f3f653c8
heitor2002 <montagnini.heitor@gmail.com>
2023-04-20 19:02:23 -0300

    feat: recebendo dados do formulário front-end.

--- back-projeto-arquitetura/index.js
+++ back-projeto-arquitetura/index.js
@@ -1,23 +1,21 @@
 const express = require("express");
-const mysql = require("mysql");
-const bodyParser = require("body-parser");
+const mysql = require("mysql2");
 const app = express();
 const cors = require("cors");
+
+app.use(express.json())
 
 const port = 5000;
 
 const db = require("./models/db")
+const Client = require("./models/Client")
 
-//BODY-PARSER
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // IMPLEMENTAÇÃO DO CORS
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "X-PINGOTHER , Content-Type, Authorization"
@@ -26,7 +24,14 @@ app.use((req, res, next) => {
   next();
 });
 
-
+app.post("/orcamento", async (req,res) => {
+    console.log(req.body);
+    
+    return res.json({
+        error: false,
+        message: "Cadastro enviado com sucesso!"
+    })
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
