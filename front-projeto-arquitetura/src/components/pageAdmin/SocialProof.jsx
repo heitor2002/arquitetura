580c4dfdbd30a4d4d57cbe2c340d902010310790
heitor2002 <montagnini.heitor@gmail.com>
2023-04-25 19:25:06 -0300

    feat: adicionando prova social ao banco de dados.

--- front-projeto-arquitetura/src/components/pageAdmin/SocialProof.jsx
+++ front-projeto-arquitetura/src/components/pageAdmin/SocialProof.jsx
@@ -1,20 +1,45 @@
 import fetchApi from "@/hooks/fetchApi";
+import { useState } from "react";
 
 const SocialProof = () => {
-  const { data } = fetchApi("http://localhost:3000/api/socialProof");
+  const { data } = fetchApi("http://localhost:5000/social-proof");
   console.log(data);
+
+  const [dataSocialProof, setDataSocialProof] = useState({
+    name: "",
+    message: ""
+  })
+
+  const onChangeInput = (e) => {
+    setDataSocialProof({...dataSocialProof, [e.target.name]: e.target.value})
+  }
+
+  const handleSubmitAddSocialProof = async () => {
+    try{
+      fetch("http://localhost:5000/social-proof", {
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(dataSocialProof)
+      }).then(() => {
+        window.location.reload()
+      })
+    }catch(err){
+      console.log(err)
+    }
+  }
+
   return (
     <>
       <div className="container-register">
         <h2>Prova social:</h2>
-        <form className="form-social-proof">
+        <form className="form-social-proof" onSubmit={handleSubmitAddSocialProof}>
           <div>
             <label>Nome:</label>
-            <input type="text" required />
+            <input type="text" required name="name" value={dataSocialProof.name} onChange={onChangeInput}/>
           </div>
           <div>
             <label>Mensagem:</label>
-            <textarea required></textarea>
+            <textarea required name="message" value={dataSocialProof.message} onChange={onChangeInput}></textarea>
           </div>
           <div>
             <input type="submit" value={"Enviar"} />
@@ -30,6 +55,19 @@ const SocialProof = () => {
                   <button className="delete-message">Excluir</button>
                   <button className="edit-message">Editar</button>
                 </div>
+                <form className="edit-form">
+                  <div>
+                  <label>Nome</label>
+                  <input type="text" name="name" />
+                  </div>
+                  <div>
+                  <label>Mensagem</label>
+                  <textarea name="message"></textarea>
+                  </div>
+                  <div>
+                    <input type="submit" value={"Atualizar"}/>
+                  </div>
+                </form>
               </div>
             </>
           );
