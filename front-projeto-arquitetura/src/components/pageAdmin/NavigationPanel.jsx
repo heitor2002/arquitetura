c06e0319e3dc6f6e29b7788d13732f79838913fa
heitor2002 <montagnini.heitor@gmail.com>
2023-04-29 20:37:58 -0300

    feat: sistema de logout.

--- front-projeto-arquitetura/src/components/pageAdmin/NavigationPanel.jsx
+++ front-projeto-arquitetura/src/components/pageAdmin/NavigationPanel.jsx
@@ -1,9 +1,24 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { ImExit } from "react-icons/im";
 
 const NavigationPanel = () => {
+  const router = useRouter()
+  const logOut = (e) => {
+    cookieStore.getAll().then((cookies) =>
+      cookies.forEach((cookie) => {
+        cookieStore.delete(cookie.name);
+      })
+    ).then(() => {
+      router.push("/")
+    })
+  };
   return (
     <>
       <div className="buttons">
+      <Link href={"/"}>
+          <button>Página Inicial</button>
+        </Link>
         <Link href={"/secret-page/portfolioEdit"}>
           <button>Editar portfólio</button>
         </Link>
@@ -12,6 +27,17 @@ const NavigationPanel = () => {
         </Link>
         <Link href={"/secret-page/clientes"}>
           <button>Cadastros</button>
+        </Link>
+        <Link href={"javascript:void(0)"}>
+          <button
+            style={{
+              backgroundColor: "#e85941",
+              color: "white",
+            }}
+            onClick={logOut}
+          >
+            Fazer logout <ImExit />
+          </button>
         </Link>
       </div>
     </>
